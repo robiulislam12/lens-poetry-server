2b381010ff38fff5b9ad5188a2def8e3a83044cf
Robiul Islam <robiul215565@gmail.com>
2022-11-09 05:07:57 +0600

    Services get and post route done

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Lens Poetry - Wild PhotoGrapy Agency 
 
 ## Server side code
+
+## Using Technology
+- Node Js
+- Mongo DB
+- Dotenv 
+- Cors
+- Nodemon 
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // External import
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require("cors");
 require('dotenv').config();
 
@@ -10,6 +11,42 @@ const port = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+// Connect mongodb
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_PASSWORD}@cluster1.ftnnc4j.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+  try{
+    const database = client.db('lensPoetryDB');
+    const servicesCollection = database.collection('services');
+
+    // GET services
+    app.get('/services', async(req, res)=>{
+      const service = await servicesCollection.find({}).toArray();
+      res.send(service);
+    })
+    // GET services only 3 items
+    app.get('/servicesItem', async(req, res)=>{
+      const service = await servicesCollection.find({}).limit(3).toArray();
+      res.send(service);
+    })
+    // POST services
+    app.post('/services', async(req, res)=>{
+      const data = req.body;
+      const service = await servicesCollection.insertOne(data);
+      res.send(service);
+    })
+
+
+  }
+  finally{
+    // await client.close();
+  }
+}
+
+run().catch(err => console.dir(err))
 
 // Routes
 app.get("/health", (req, res) => {
